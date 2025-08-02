f4bbb93f3381dfb4489038e6f62a72b2ae4412db
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-02 23:58:16 +0300

    Added allow from allow to validation

--- backend/handlers/dates.go
+++ backend/handlers/dates.go
@@ -4,6 +4,8 @@ import (
 	"dateChoice/models"
 	"dateChoice/schemas"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/lib/pq"
@@ -11,6 +13,29 @@ import (
 
 func (h *Handler) DateChoiceHandler(c echo.Context) error {
 	dates := c.Get("validatedBody").(*schemas.DateChoiceRequest)
+
+	allowFrom := os.Getenv("ALLOW_FROM")
+	allowTo := os.Getenv("ALLOW_TO")
+
+	layout := "2006-01-02"
+	allowFromTime, err := time.Parse(layout, allowFrom)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, schemas.Error{Error: "Invalid ALLOW_FROM format"})
+	}
+	allowToTime, err := time.Parse(layout, allowTo)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, schemas.Error{Error: "Invalid ALLOW_TO format"})
+	}
+
+	for _, dateStr := range dates.Dates {
+		dateTime, err := time.Parse(layout, dateStr)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, schemas.Error{Error: "Invalid date format (YYYY-MM-DD)"})
+		}
+		if dateTime.Before(allowFromTime) || dateTime.After(allowToTime) {
+			return c.JSON(http.StatusBadRequest, schemas.Error{Error: "Date out of allowed range (YYYY-MM-DD)"})
+		}
+	}
 
 	var userChoice models.UserChoice
 	if err := h.DB.First(&userChoice, "user_id = ?", c.Get("userID")).Error; err != nil {
