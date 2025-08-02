d65cf8b6eef5f42bf48cb8d79837887d7ffe83e6
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-02 18:21:40 +0300

    Check auth

---
+++ frontend/src/components/Error.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+        <span class="block sm:inline">{{ props.error }}</span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['error'])
+</script>
--- frontend/src/views/DateChose.vue
+++ frontend/src/views/DateChose.vue
@@ -1,17 +1,25 @@
 <template>
   <div>{{ initdata }}</div>
   <div> {{ initdata?.user?.username }}</div>
-  <div>
+  <div v-if="authorized">
     <Calendar />
+  </div>
+  <div v-else>
+    <Error error="Кажется вы не заходите не через телеграм, обновите страничку или откройте приложение через телеграм" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import Calendar from '@/components/Calendar.vue';
+import Error from '@/components/Error.vue';
 
 const initdata = ref();
+const authorized = ref(false);
 
 let tg = window.Telegram.WebApp;
 initdata.value = tg.initDataUnsafe;
+if (tg.initDataUnsafe && tg.initDataUnsafe.user && tg.initDataUnsafe.user.id) {
+  authorized.value = true;
+}
 </script>
