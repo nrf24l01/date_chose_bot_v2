429ba1d3912cb33feb2002294f538a443d601d5c
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-02 16:32:44 +0300

    tg support

--- frontend/src/views/DateChose.vue
+++ frontend/src/views/DateChose.vue
@@ -1,9 +1,17 @@
 <template>
+  <div>{{ initdata }}</div>
+  <div> {{ initdata?.user?.username }}</div>
   <div>
     <Calendar />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import Calendar from '@/components/Calendar.vue';
+
+const initdata = ref();
+
+let tg = window.Telegram.WebApp;
+initdata.value = tg.initDataUnsafe;
 </script>
